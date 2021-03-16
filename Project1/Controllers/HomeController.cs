ba0a6146afc967b1afdb741b8342c4bd168dc424
Brandon <bhatch96@gmail.com>
2021-03-15 21:31:28 -0600

    Home page and some controlling

--- Project1/Controllers/HomeController.cs
+++ Project1/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Project1.Controllers
             return View();
         }
 
+        public IActionResult EnterAppointment()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
